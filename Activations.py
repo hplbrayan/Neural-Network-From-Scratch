9e5806d83080951b0753795ed7aa8b8bba984624
hplbrayan <hplbrayan@gmail.com>
2024-07-26 15:38:43 -0500

    Act 1.1 ReLu + linear

--- Activations.py
+++ Activations.py
@@ -1,1 +1,33 @@
 import numpy as np
+
+class Activations:
+    
+    def __init__(self, act='relu'):
+        
+        if act=='relu':
+            self.act = self.relu
+            self.dadz = self.d_relu
+            
+        if act=='identity':
+            self.act = self.identity
+            self.dadz = self.d_identity
+            
+            
+    #===============================================================================#
+    #                                        ReLU                                   #
+    #===============================================================================#
+    def relu(self, x):
+        return np.where(np.array(x)>0, x, 0)
+    
+    def d_relu(self, x):
+        return np.where(np.array(x)>0, 1, 0)
+    
+    #===============================================================================#
+    #                                      Identity                                 #
+    #===============================================================================#
+    def identity(self, x):
+        return np.where(np.array(x)==np.array(x), x, 0)
+    
+    def d_identity(self, x):
+        return np.where(np.array(x)==np.array(x), 1, 0)
+   
